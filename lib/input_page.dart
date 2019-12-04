af489ff4dea68a164db0e3ee3b7388da47a6c325
TsengYiYao <yiyatsen@gmail.com>
2019-12-04 11:38:52 +0800

    use @required in constructor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,60 +20,25 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-//                    height: 200,
-                    ),
+                    child: ReusableCard(color: Color(0XFF1D1E33)),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-//                    height: 200,
-                    ),
+                    child: ReusableCard(color: Color(0XFF1D1E33)),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Color(0XFF1D1E33),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-//                height: 200,
-              ),
+              child: ReusableCard(color: Color(0XFF1D1E33)),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-//                    height: 200,
-                    ),
+                    child: ReusableCard(color: Color(0XFF1D1E33)),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Color(0XFF1D1E33),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-//                    height: 200,
-                    ),
+                    child: ReusableCard(color: Color(0XFF1D1E33)),
                   ),
                 ],
               ),
@@ -81,12 +46,23 @@ class _InputPageState extends State<InputPage> {
           ],
         ),
       ),
-//      floatingActionButton: Theme(
-//        data: ThemeData(accentColor: Colors.purple),
-//        child: FloatingActionButton(
-//          child: Icon(Icons.add),
-//        ),
-//      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.color});
+
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
