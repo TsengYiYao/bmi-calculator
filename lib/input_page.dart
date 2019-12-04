8761cd646be9f2fc6802e57a4dabd4ef61d7277d
TsengYiYao <yiyatsen@gmail.com>
2019-12-04 10:42:00 +0800

    add input_page.dart extract class InputPage

---
+++ lib/input_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+//      floatingActionButton: Theme(
+//        data: ThemeData(accentColor: Colors.purple),
+//        child: FloatingActionButton(
+//          child: Icon(Icons.add),
+//        ),
+//      ),
+    );
+  }
+}
